71f80c5eff800b5d2c93b0cc86f69e6cb2856110
omic <esther.zh.wenting@gmail.com>
2015-09-13 15:45:38 -0400

    init-web api practice-exercise 1 http://www.asp.net/web-api/overview/older-versions/build-restful-apis-with-aspnet-web-api#Exercise1

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using ContactManager.Models;
+using ContactManager.Services;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactRepository contactRepository;
+        
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
